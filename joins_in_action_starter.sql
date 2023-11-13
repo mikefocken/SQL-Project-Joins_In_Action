9fba4aabd1b0b373785860b95f63412697efa859
mike focken <mikefocken@gmail.com>
2023-11-13 06:58:53 -0800

    saved work

--- joins_in_action_starter.sql
+++ joins_in_action_starter.sql
@@ -2,39 +2,47 @@
 -- Do a full outer join to see all records from both tables matched where possible.
 -- EXPECTED RESULT: 77 Rows Returned
 
-SELECT * FROM movies LEFT JOIN users on movie_id = favorite_movie_id
-UNION
-SELECT * FROM movies RIGHT JOIN users on movie_id = favorite_movie_id;
+SELECT * FROM movies LEFT JOIN users on  favorite_movie_id= movie_id;
+Union
+SELECT * FROM movies RIGHT JOIN users on movie_id=favorite_movie_id;
 
 -- Because MySQL does not support "OUTER JOIN" syntax, we must use left and right joins that are brought together with "UNION".
 
 -- <<<<<<<<<<<<<<<<<<<<<< PROBLEM 1 >>>>>>>>>>>>>>>>>>>>>>>
 -- Do a left join to see all records from the movies table and matching records from the users table.
 -- EXPECTED RESULT: 75 Rows Returned
+SELECT * FROM movies LEFT JOIN users on movie_id = user_id;
 
-select *
 -- <<<<<<<<<<<<<<<<<<<<<< PROBLEM 2 >>>>>>>>>>>>>>>>>>>>>>>
 -- Do a right join to see all records from the users table and matching records from the movies table.
 -- EXPECTED RESULT: 16 Rows Returned
-
+SELECT* FROM movies right JOIN users on user_id = movie_id;
 
 -- <<<<<<<<<<<<<<<<<<<<<< PROBLEM 3 >>>>>>>>>>>>>>>>>>>>>>>
 -- Do a inner join to get only the rows from the movies tables that have matching rows in the users tables.
 -- EXPECTED RESULT: 14 Rows Returned
-
-
+SELECT count(*) FROM movies inner join users on favorite_movie_id = movie_id;
 -- <<<<<<<<<<<<<<<<<<<<<< PROBLEM 4 >>>>>>>>>>>>>>>>>>>>>>>
 -- Determine which lead studio's movies are favorited by users the most.
 -- EXPECTED RESULT: Disney
+SELECT movies.lead_studio, COUNT(*) AS count_of_occurrences FROM movies
+JOIN users ON movies.movie_id = users.favorite_movie_id GROUP BY movies.lead_studio ORDER BY count_of_occurrences desc limit 1;
+
+
 
 
 -- <<<<<<<<<<<<<<<<<<<<<< PROBLEM 5 >>>>>>>>>>>>>>>>>>>>>>>
 -- Get the average Rotten Tomatoes score of all movies that are favorited by a user.
 -- EXPECTED RESULT: 52.21
+-- Get the average Rotten Tomatoes score of all movies that are favorited by a user.
+SELECT AVG(movies.Rotten_Tomatoes) AS rotten_tomatoes
+FROM movies
+JOIN users ON movies.movie_id = users.favorite_movie_id;
+
 
 
 -- <<<<<<<<<<<<<<<<<<<<<< PROBLEM 6 >>>>>>>>>>>>>>>>>>>>>>>
 -- Determine which two movies are have duplicate entries in the dataset. (HINT: No join is needed for this problem)
 -- EXPECTED RESULT: Mamma Mia!, Gnomeo and Juliet
 -- It is important to be able to identify dirty data within a dataset!
-
+select film, count(*) as duplicate_count from movies group by movies.film having Count(*) >1;
